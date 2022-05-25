60d051c6f0e18b6593168aa392e5a99211869c22
gbouras13 <george.bouras@adelaide.edu.au>
2022-05-25 10:59:16 +0930

    slurm2

--- slurm/run_e_coli.sh
+++ slurm/run_e_coli.sh
@@ -24,7 +24,7 @@ cd $SNK_DIR
 module load Anaconda3/2020.07
 conda activate snakemake_clean_env
 
- snakemake -c 1 -s runner.smk --use-conda --config Fastas='/hpcfs/users/a1667917/s_aureus/e_coli_fasta' \
+ snakemake -c 1 -s runner.smk --use-conda --profile $PROF_DIR/bact_assembly  --config Fastas='/hpcfs/users/a1667917/s_aureus/e_coli_fasta' \
  Output=/hpcfs/users/a1667917/s_aureus/e_coli_motif Motif='AGCGCAAGTA' --conda-frontend conda 
 
 
